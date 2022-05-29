55f47651453a44647d34c514802deb7d777e0c33
napthedev <phongna.dev@gmail.com>
2022-05-29 12:00:17 +0700

    Add config file for heroku

--- server.js
+++ server.js
@@ -406,6 +406,7 @@ app.use((req, res) => {
     res.redirect('/404')
 })
 
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 })
